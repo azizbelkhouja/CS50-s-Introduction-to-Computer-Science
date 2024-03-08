cc5069d4da7f577812c90b2b8042c723504d5e36
azizbelkhouja <belkhoujamohamedaziz@gmail.com>
2024-03-08 15:06:39 +0100

    version one

--- credit/credit.c
+++ credit/credit.c
@@ -7,7 +7,7 @@ int main(void) {
 
     char cardnumber[16];
 
-    printf("Number3: ");
+    printf("Number5: ");
 
     while(1) 
     {
@@ -15,41 +15,55 @@ int main(void) {
         scanf("%s", cardnumber);
         int length = strlen(cardnumber);
 
+
+        //check if all input is integers
         for (int i=0; i<length; i++) 
         {
             if (!isdigit(cardnumber[i]))
             {
                 while (getchar() != '\n');
                 printf("Number: ");
-            }
+            } else continue;
         }
 
 
 
         //Card type check
-        if ( (length == 15)) {
-            if ((cardnumber[0] - '0') == 3) {
-                if ((cardnumber[1] - '0') == 4 || (cardnumber[1] - '0') == 7) {
+        if ( (length == 15)) 
+        {
+            if ((cardnumber[0] - '0') == 3) 
+            {
+                if ((cardnumber[1] - '0') == 4 || (cardnumber[1] - '0') == 7) 
+                {
                     printf("American Express\n");
                     break;
-                }
-            }
-        } else  if ( (length == 16)) {
-                    if ((cardnumber[0] - '0') == 5) {
-                        if ((cardnumber[1] - '0') >= 1 || (cardnumber[1] - '0') <= 5) {
-                            //printf("VISA\n");
+                } else continue;
+            } else continue;
+        } else  if ( (length == 16)) 
+                {
+                    if ((cardnumber[0] - '0') == 5) 
+                    {
+                        if ((cardnumber[1] - '0') >= 1 || (cardnumber[1] - '0') <= 5) 
+                        {
+                            printf("VISA\n");
                             break;
+                        } else continue;
+                    } else  if ((cardnumber[0] - '0') == 4) 
+                            {
+                            printf("MASTERCARD\n");
+                            break;
+                            } else continue;
+                } else  if ( (length == 13))
+                        {
+                            if ((cardnumber[0] - '0') == 4) 
+                            {
+                                printf("MASTERCARD\n");
+                                break;
+                            } else continue;
+                        } else {
+                            while (getchar() != '\n');
+                            printf("mancano digiti! Number: ");
                         }
-                    } else  if ((cardnumber[0] - '0') == 4) {
-                            //printf("MASTERCARD\n");
-                            break;
-                            }
-                } else  if ( (length == 13)) {
-                            if ((cardnumber[0] - '0') == 4) {
-                                    //printf("MASTERCARD\n");
-                                    break;
-                                }
-                            }
     }
     printf("testEND\n");
 
