5f1b1717b440bece8b448271641544607472643c
azizbelkhouja <belkhoujamohamedaziz@gmail.com>
2024-03-21 18:16:15 +0100

    brb

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -15,6 +15,8 @@ int main (int argc, char *argv[])
     char key[MAX];
     strcpy(key, argv[1]);
 
+    printf("key: %s\n", key);
+
     if (strlen(key) != 26) 
     {
         printf("key must contain 26 characters.\n");
@@ -30,6 +32,54 @@ int main (int argc, char *argv[])
         }
     }
 
-    
+    for (int j = 0; j < 26; j++)
+    {
+        for (int k = j + 1; k < 26; k++)
+        {
+            if (tolower(key[j]) == tolower(key[k]))
+            {
+                printf("key must not contain repeated characters.\n");
+                return 4;
+            }
+        }
+    }
+
+    char plaintext[50], ciphertext[50];
+    int m;
+
+    printf("plaintext: ");
+    fgets(plaintext, 50, stdin);
+
+    for (int l = 0; l < strlen(plaintext); l++) 
+    {
+        
+            if (islower(plaintext[l]))
+            {
+                for (m = 0; m < 26; m++)
+                {
+                    if (plaintext[l] == tolower(key[m]))
+                    {
+                        ciphertext[l] = tolower(key[m]);
+                    }
+                }
+            } 
+            else if (isupper(plaintext[l]))
+                {
+                    for (m = 0; m < 26; m++)
+                    {
+                        if (plaintext[l] == toupper(key[m]))
+                        {
+                            ciphertext[l] = toupper(key[m]);
+                        }
+                    }
+                }
+            else ciphertext[l] = plaintext[l];
+    }
+    ciphertext[strlen(plaintext)-1] = '\0';
+
+    printf("ciphertext: %s\n", ciphertext);
+
+
+
 
 }
