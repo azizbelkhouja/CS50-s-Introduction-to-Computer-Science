e0504aaa824f6497a6ee689db20626ba25f027a0
azizbelkhouja <belkhoujamohamedaziz@gmail.com>
2024-03-21 15:49:00 +0100

    key verification

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,10 +1,42 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main (int argc, char *argv[])
 {
+
+    int length = strlen(argv[1]);
+    int key;
+    char *str, *text;
+
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    if (argc == 2) 
+    {
+        strcpy(str, argv[1]);
+
+        for (int i = 0; i < length; i++)
+        {
+            if (!isdigit(str[i]))
+            {
+                printf("key must be only digits\n");
+                return 2;
+            }
+        }
+    }
+
+    key = atoi(str);
+
+    printf("plain text: ");
+    scanf("%s", text);
+
     
-    if (argc != 2 || !isdigit(*argv[1])) printf("Usage: ./caesar key");
+
 
 
 }
